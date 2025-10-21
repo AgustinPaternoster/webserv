308d3792ccad34c73d42e2d493b46a9d2c9a5692
agustin paternoster <agustinpaternoster@gmail.com>
2025-10-21 16:29:59 +0200

    setup working enviroment

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,5 @@
-#include "test.hpp"
-#include <iostream>
 
 int main(int ar, char** argv)
 {
-    
-    (void)ar;
-    (void)argv;   
-    test a(4);
-    std::cout << a.getNb() << std::endl;
-        std::cout << "test" << std::endl;
     return (0);
 }
