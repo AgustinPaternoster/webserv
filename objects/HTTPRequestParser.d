35f4a6867e4db1bad7b6099b7db8d6df26e6a76f
Agustin <agustinpaternoste@gmail.com>
2025-11-14 13:04:53 +0100

    clean unused files

--- objects/HTTPRequestParser.d
+++
@@ -1,6 +0,0 @@
-objects/HTTPRequestParser.o: src/HTTPRequestParser.cpp \
- include/HTTPRequestParser.hpp include/HTTPRequest.hpp \
- include/HTTPHeaders.hpp
-include/HTTPRequestParser.hpp:
-include/HTTPRequest.hpp:
-include/HTTPHeaders.hpp:
