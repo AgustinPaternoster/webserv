a364aa151be2132cf7e300f7ad14cc7076e5aa0a
Agustin <agustinpaternoste@gmail.com>
2025-12-13 17:11:56 +0100

    clean unsed files

--- objects/CgiTasks.d
+++
@@ -1,4 +0,0 @@
-objects/CgiTasks.o: src/CgiTasks.cpp include/CgiTasks.hpp \
- include/Common.hpp
-include/CgiTasks.hpp:
-include/Common.hpp:
--- objects/HTTPRequest.d
+++
@@ -1,7 +0,0 @@
-objects/HTTPRequest.o: src/HTTPRequest.cpp include/HTTPRequest.hpp \
- include/HTTPHeaders.hpp include/HTTPRequestParser.hpp \
- include/HTTPRequest.hpp
-include/HTTPRequest.hpp:
-include/HTTPHeaders.hpp:
-include/HTTPRequestParser.hpp:
-include/HTTPRequest.hpp:
